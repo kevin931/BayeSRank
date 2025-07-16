2e1958b7d87321914e34135c1383bc4384b1fb18
Kevin Wang <1776869601@qq.com>
2025-07-16 17:04:20 -0400

    Add documentation skeletons

--- R/BayeSRank.R
+++ R/BayeSRank.R
@@ -1,2 +1,11 @@
-BayeSRank <- function(){
+#' BayeSRank: Bias-Aware Rank Aggregation
+#'
+#' This is the bespoke BayeSRank algorithm introduced in Ruan et al (XXXX) paper.
+#' This implementation does not contain the two-step procedure. If you love
+#' two-stepping, please use XXX.
+#'
+#' @param x This is an
+#' @seealso The two-step function.
+#' @export
+BayeSRank <- function(x){
 }
